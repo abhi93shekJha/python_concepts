0855867bf567639e36244f61042271ea22aedfc3
abhi93shekJha <32580340+abhi93shekJha@users.noreply.github.com>
2024-01-21 23:18:03 +0530

    Update buy_item.py

--- python_modules/buy_item.py
+++ python_modules/buy_item.py
@@ -9,5 +9,5 @@ def divide(num1, num2):
     return (num1/num2)    
 
 
-# this will be printed when this module is imported, and the importing module is run- "ecommerce.py" in this case.
+# this will be printed when this module is imported, and the importing module is run- "buy_item.py" in this case.
 buy_items_from_cart()
